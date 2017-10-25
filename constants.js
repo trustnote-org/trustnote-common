955dd594f097ac017248b541041ac786412dd9f6
VictorZhang <victor.zhang@thingtrust.com>
2017-10-25 10:14:54 +0800

    Trustgraph common init

---
+++ constants.js
@@ -0,0 +1,34 @@
+/*jslint node: true */
+"use strict";
+
+exports.COUNT_WITNESSES = 12;
+exports.MAX_WITNESS_LIST_MUTATIONS = 1;
+exports.TOTAL_WHITEBYTES = 1e15;
+exports.MAJORITY_OF_WITNESSES = (exports.COUNT_WITNESSES%2===0) ? (exports.COUNT_WITNESSES/2+1) : Math.ceil(exports.COUNT_WITNESSES/2);
+exports.COUNT_MC_BALLS_FOR_PAID_WITNESSING = 100;
+
+exports.version = '1.0';
+exports.alt = '1';
+
+exports.GENESIS_UNIT = (exports.alt === '2' && exports.version === '1.0t') ? 'TvqutGPz3T4Cs6oiChxFlclY92M2MvCvfXR5/FETato=' : 'oj8yEksX9Ubq7lLc+p6F2uyHUuynugeVq4+ikT67X6E=';
+exports.BLACKBYTES_ASSET = (exports.alt === '2' && exports.version === '1.0t') ? 'LUQu5ik4WLfCrr8OwXezqBa+i3IlZLqxj2itQZQm8WY=' : 'qO2JsiuDMh/j+pqJYZw3u82O71WjCDf0vTNvsnntr8o=';
+
+exports.HASH_LENGTH = 44;
+exports.PUBKEY_LENGTH = 44;
+exports.SIG_LENGTH = 88;
+
+// anti-spam limits
+exports.MAX_AUTHORS_PER_UNIT = 16;
+exports.MAX_PARENTS_PER_UNIT = 16;
+exports.MAX_MESSAGES_PER_UNIT = 128;
+exports.MAX_SPEND_PROOFS_PER_MESSAGE = 128;
+exports.MAX_INPUTS_PER_PAYMENT_MESSAGE = 128;
+exports.MAX_OUTPUTS_PER_PAYMENT_MESSAGE = 128;
+exports.MAX_CHOICES_PER_POLL = 128;
+exports.MAX_DENOMINATIONS_PER_ASSET_DEFINITION = 64;
+exports.MAX_ATTESTORS_PER_ASSET = 64;
+exports.MAX_DATA_FEED_NAME_LENGTH = 64;
+exports.MAX_DATA_FEED_VALUE_LENGTH = 64;
+exports.MAX_AUTHENTIFIER_LENGTH = 4096;
+exports.MAX_CAP = 9e15;
+exports.MAX_COMPLEXITY = 100;
---
+++ enforce_singleton.js
@@ -0,0 +1,7 @@
+/*jslint node: true */
+"use strict";
+
+if (global._bTrustgraphCommonLoaded)
+	throw Error("Looks like you are loading multiple copies of TrustgraphCommon, which is not supported.\nRunnung 'npm dedupe' might help.");
+
+global._bTrustgraphCommonLoaded = true;
