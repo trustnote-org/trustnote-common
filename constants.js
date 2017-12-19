b825c321f45b2e625761cc0186430cbaf72ab5c9
VictorZhang <victor.zhang@thingtrust.com>
2017-12-19 14:50:49 +0800

    modi genesis unit

--- constants.js
+++ constants.js
@@ -10,7 +10,7 @@ exports.COUNT_MC_BALLS_FOR_PAID_WITNESSING = 100;
 exports.version = '1.0';
 exports.alt = '1';
 
-exports.GENESIS_UNIT = '1DuU7KzFKHHj0DXq5BdzFpYWwrfC2TK94ot+AbxLEpw=';
+exports.GENESIS_UNIT = 'h5w0C5A+HF+BjsbEgqmcny0uK2coR3XAZLUDJKYWUxQ=';
 exports.BLACKBYTES_ASSET = '9qQId3BlWRQHvVy+STWyLKFb3lUd0xfQhX6mPVEHC2c=';
 
 exports.HASH_LENGTH = 44;
