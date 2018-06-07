06227922d09f0f482f747c4ceaab2431c76fc6d5
Victor Zhang <victor55511@163.com>
2018-06-07 09:46:56 +0800

    Revert "Update wallet_defined_by_addresses.js"

--- wallet_defined_by_addresses.js
+++ wallet_defined_by_addresses.js
@@ -502,4 +502,3 @@ exports.readSharedAddressDefinition = readSharedAddressDefinition;
 exports.determineIfHasMerkle = determineIfHasMerkle;
 exports.createNewSharedAddress = createNewSharedAddress;
 exports.createNewSharedAddressByTemplate = createNewSharedAddressByTemplate;
-exports.sendNewSharedAddress = sendNewSharedAddress;
