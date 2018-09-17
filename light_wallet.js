7e188164a5b64ce4a3e03f2c7a0762c15dd098ea
kake <gusuncheng@gmail.com>
2018-09-17 13:43:51 +0800

    modi connect network

--- light_wallet.js
+++ light_wallet.js
@@ -8,23 +8,44 @@ var walletGeneral = require('./wallet_general.js');
 var light = require('./light.js');
 var eventBus = require('./event_bus.js');
 
-var RECONNECT_TO_LIGHT_VENDOR_PERIOD = 60*1000;
+var RECONNECT_TO_LIGHT_VENDOR_PERIOD = 60 * 1000;
 var refreshLightTimeoutDoneCount = 0;
 
-
-function setLightVendorHost(light_vendor_host){
-	network.light_vendor_url = conf.WS_PROTOCOL+light_vendor_host; // for now, light vendor is also a hub
-	if (conf.bLight){
-		refreshLightClientHistory();
+function setLightVendorHost(light_vendor_host) {
+	network.light_vendor_url = conf.WS_PROTOCOL + light_vendor_host; // for now, light vendor is also a hub
+	if (conf.bLight) {
+		// refreshLightClientHistory();
+		connecteToLightVendor();
 		setInterval(reconnectToLightVendor, RECONNECT_TO_LIGHT_VENDOR_PERIOD);
-		eventBus.on('connected', reconnectToLightVendor);
+		// eventBus.on('connected', reconnectToLightVendor);
 	}
 }
 
-function reconnectToLightVendor(){
-	network.findOutboundPeerOrConnect(network.light_vendor_url, function(err, ws){
+function connecteToLightVendor() {
+	if (!conf.bLight)
+		return;
+	if (!network.light_vendor_url)
+		return console.log('refreshLightClientHistory called too early: light_vendor_url not set yet');
+	network.findOutboundPeerOrConnect(network.light_vendor_url, function (err, ws) {
+		var finish = function (msg) {
+			if (msg) {
+				refreshLightTimeoutDoneCount++;
+				if (refreshLightTimeoutDoneCount < 6)
+					eventBus.emit('refresh_light_timeout');
+			}
+			if (ws)
+				ws.bRefreshingHistory = false;
+		};
 		if (err)
-			return console.log("reconnectToLightVendor: "+err);
+			return finish("refreshLightClientHistory: " + err);
+		console.log('refreshLightClientHistory connected');
+	})
+}
+
+function reconnectToLightVendor() {
+	network.findOutboundPeerOrConnect(network.light_vendor_url, function (err, ws) {
+		if (err)
+			return console.log("reconnectToLightVendor: " + err);
 		if (ws.bLightVendor)
 			return console.log("already connected to light vendor");
 		if (ws.bRefreshingHistory)
@@ -33,23 +54,23 @@ function reconnectToLightVendor(){
 	});
 }
 
-function readListOfUnstableUnits(handleUnits){
-	db.query("SELECT unit FROM units WHERE is_stable=0", function(rows){
-		var arrUnits = rows.map(function(row){ return row.unit; });
+function readListOfUnstableUnits(handleUnits) {
+	db.query("SELECT unit FROM units WHERE is_stable=0", function (rows) {
+		var arrUnits = rows.map(function (row) { return row.unit; });
 		handleUnits(arrUnits);
 	});
 }
 
 
-function prepareRequestForHistory(handleResult){
-	myWitnesses.readMyWitnesses(function(arrWitnesses){
+function prepareRequestForHistory(handleResult) {
+	myWitnesses.readMyWitnesses(function (arrWitnesses) {
 		if (arrWitnesses.length === 0) // first start, witnesses not set yet
 			return handleResult(null);
-		var objHistoryRequest = {witnesses: arrWitnesses};
-		walletGeneral.readMyAddresses(function(arrAddresses){
+		var objHistoryRequest = { witnesses: arrWitnesses };
+		walletGeneral.readMyAddresses(function (arrAddresses) {
 			if (arrAddresses.length > 0)
 				objHistoryRequest.addresses = arrAddresses;
-			readListOfUnstableUnits(function(arrUnits){
+			readListOfUnstableUnits(function (arrUnits) {
 				if (arrUnits.length > 0)
 					objHistoryRequest.requested_joints = arrUnits;
 				if (!objHistoryRequest.addresses && !objHistoryRequest.requested_joints)
@@ -59,12 +80,12 @@ function prepareRequestForHistory(handleResult){
 				objHistoryRequest.last_stable_mci = 0;
 				var strAddressList = arrAddresses.map(db.escape).join(', ');
 				db.query(
-					"SELECT unit FROM unit_authors JOIN units USING(unit) WHERE is_stable=1 AND address IN("+strAddressList+") \n\
+					"SELECT unit FROM unit_authors JOIN units USING(unit) WHERE is_stable=1 AND address IN(" + strAddressList + ") \n\
 					UNION \n\
-					SELECT unit FROM outputs JOIN units USING(unit) WHERE is_stable=1 AND address IN("+strAddressList+")",
-					function(rows){
+					SELECT unit FROM outputs JOIN units USING(unit) WHERE is_stable=1 AND address IN("+ strAddressList + ")",
+					function (rows) {
 						if (rows.length)
-							objHistoryRequest.known_stable_units = rows.map(function(row){ return row.unit; });
+							objHistoryRequest.known_stable_units = rows.map(function (row) { return row.unit; });
 						handleResult(objHistoryRequest);
 					}
 				);
@@ -82,17 +103,17 @@ function prepareRequestForHistory(handleResult){
 }
 
 
-function refreshLightClientHistory(){
+function refreshLightClientHistory() {
 	if (!conf.bLight)
 		return;
 	if (!network.light_vendor_url)
 		return console.log('refreshLightClientHistory called too early: light_vendor_url not set yet');
 	eventBus.emit('refresh_light_started');
-	network.findOutboundPeerOrConnect(network.light_vendor_url, function onLocatedLightVendor(err, ws){
-		var finish = function(msg){
+	network.findOutboundPeerOrConnect(network.light_vendor_url, function onLocatedLightVendor(err, ws) {
+		var finish = function (msg) {
 			if (msg) {
 				refreshLightTimeoutDoneCount++;
-				if(refreshLightTimeoutDoneCount < 6)
+				if (refreshLightTimeoutDoneCount < 6)
 					eventBus.emit('refresh_light_timeout');
 			}
 			if (ws)
@@ -100,29 +121,29 @@ function refreshLightClientHistory(){
 			eventBus.emit('refresh_light_done');
 		};
 		if (err)
-			return finish("refreshLightClientHistory: "+err);
+			return finish("refreshLightClientHistory: " + err);
 		console.log('refreshLightClientHistory connected');
 		// handling the response may take some time, don't send new requests
 		if (ws.bRefreshingHistory)
 			return console.log("previous refresh not finished yet");
 		ws.bRefreshingHistory = true;
-		prepareRequestForHistory(function(objRequest){
+		prepareRequestForHistory(function (objRequest) {
 			if (!objRequest)
 				return finish();
-			network.sendRequest(ws, 'light/get_history', objRequest, false, function(ws, request, response){
+			network.sendRequest(ws, 'light/get_history', objRequest, false, function (ws, request, response) {
 				if (response.error)
 					return finish(response.error);
 				ws.bLightVendor = true;
-				var interval = setInterval(function(){ // refresh UI periodically while we are processing history
+				var interval = setInterval(function () { // refresh UI periodically while we are processing history
 					eventBus.emit('maybe_new_transactions');
 				}, 500);
 				light.processHistory(response, {
-					ifError: function(err){
+					ifError: function (err) {
 						clearInterval(interval);
 						network.sendError(ws, err);
 						finish();
 					},
-					ifOk: function(){
+					ifOk: function () {
 						clearInterval(interval);
 						finish();
 						eventBus.emit('maybe_new_transactions');
